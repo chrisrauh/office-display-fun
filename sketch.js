1ac329cdf542695144272d4fac5d9ee5d9e19a85
Christian Rauh <crauh@uber.com>
2019-06-27 09:20:18 -0700

    Sense Making

--- sketch.js
+++ sketch.js
@@ -1,24 +1,56 @@
-let numBalls = 13;
+
+let numBalls = 16;
 let spring = 0.05;
-let gravity = 0.03;
+let gravity = 0.01;
 let friction = -0.9;
 let balls = [];
 
+let borderSize = 5;
+
+let pallete = [[45, 44, 45], [91, 84, 81], [164, 126, 111], [237, 206, 181], [217, 168, 123]];
+let palleteLoaded = false;
+
 function setup() {
+
+  var url = "http://colormind.io/api/";
+  var data = {
+    model : "default"
+  }
+
+  fetch(url, {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+  }).then(response => response.json())
+    .then(responsejson => responsejson.result)
+    .then(result => {
+      pallete = result;
+      palleteLoaded = true;
+      console.log('Success:', pallete);
+      setupAfterLoad();
+    })
+    .catch(error => console.error('Error:', error));
+
+}
+
+function setupAfterLoad() {
+
   var cnv = createCanvas(windowWidth - 100, windowHeight - 100);
   cnv.style('display', 'block');
-  blendMode(OVERLAY);
   for (let i = 0; i < numBalls; i++) {
+    colorIndex = i%(pallete.length-1)+1
     balls[i] = new Ball(
-        random(width),
-        random(height),
-        random(30, 70),
-        i,
-        balls,
+      random(width),
+      random(height),
+      random(10, 60),
+      i,
+      balls,
+      pallete[colorIndex],
     );
   }
+
   noStroke();
-  fill(255, 204);
+  background(...pallete[0]);
+
 }
 
 function mousePressed() {
@@ -35,41 +67,29 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-const randoColor = () => Math.floor(Math.random() * 255) + 1;
-const backgroundColor = [
-  randoColor(),
-  randoColor(),
-  randoColor(),
-  Math.max(.1, Math.random())
-];
-const ballColor = () =>  [
-  randoColor(),
-  randoColor(),
-  randoColor(),
-];
-
+const randoColor = () => pallete[Math.floor(Math.random() * (pallete.length - 1)) + 1];
 
 function draw() {
-  background(...backgroundColor);
-  balls.forEach(ball => {
-    ball.collide();
-    ball.move();
-    ball.display();
-    fill(...ballColor());
-    ball.diameter -= .1;
-  });
+  if (palleteLoaded) {
+    balls.forEach(ball => {
+      ball.collide();
+      ball.move();
+      ball.display();
+    });
+  }
 
 }
 
 class Ball {
-  constructor(xin, yin, din, idin, oin) {
+  constructor(xin, yin, din, idin, oin, color) {
     this.x = xin;
     this.y = yin;
-    this.vx = 0;
-    this.vy = 0;
+    this.vx = 3;
+    this.vy = -0.1;
     this.diameter = din;
     this.id = idin;
     this.others = oin;
+    this.color = color;
   }
 
   collide() {
@@ -113,6 +133,7 @@ class Ball {
   }
 
   display() {
+    fill(this.color);
     ellipse(this.x, this.y, this.diameter, this.diameter);
   }
 }
