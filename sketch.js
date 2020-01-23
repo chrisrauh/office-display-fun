9af8a4602df4e04aaa4de65ed7d16c0b860b1407
Joshua Lee <neuralism@gmail.com>
2020-01-23 03:48:11 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 
-let numBalls = 10;
+let numBalls = 50;
 let spring = 0.1;
-let gravity = 0.5;
+let gravity = 0.01; // 0.5
 let friction = -.95;
 let balls = [];
 
-let borderSize = 30;
+let borderSize = 0;
 
 let pallete = [[45, 44, 45], [91, 84, 81], [164, 126, 111], [237, 206, 181], [217, 168, 123]];
 let palleteLoaded = false;
@@ -16,7 +16,7 @@ let pg1;
 let img = [];
 
 let rotationVal = 0;
-let rotationBaseSpeed = 0.01;
+let rotationBaseSpeed = (Math.PI / 180) * 1;
 let rotationSpeed = rotationBaseSpeed;
 
 const Y_AXIS = 1;
@@ -36,12 +36,13 @@ function setup() {
   cnv.style('display', 'block');
   noStroke();
 
-  pg0 = createGraphics(width,height);
+  pg0 = createGraphics(width, height);
   pg0.noStroke();
-
-  pg1 = createGraphics(width, height / 2);
+  // Sup
+  // pg1 = createGraphics(width, height / 2);
+  pg1 = createGraphics(width / 1.8, 500);  
   pg1.noStroke();
-  pg1.translate(0,height/2);
+  pg1.translate(0, height/2);
   pg1.scale(1, -1);
 
   var url = "http://colormind.io/api/";
@@ -76,23 +77,21 @@ function setup() {
 function setupAfterLoad() {
 
   background(...pallete[0]);
-  pg0.background(...pallete[0]);
-  pg1.background(...pallete[0]);
+  // pg0.background(...pallete[0]);
+  // pg1.background(...pallete[0]);
 
   for (let i = 0; i < numBalls; i++) {
     colorIndex = i%(pallete.length-1)+1
     balls[i] = new Ball(
-      random(width),
-      random(height),
-      random(30) ,
+      random(width), // width
+      random(height), // height
+      random(30), // 30
       i,
       balls,
       pallete[colorIndex],
       img[0]
     );
   }
-
-
 
 }
 
@@ -105,8 +104,9 @@ const randoColor = () => pallete[Math.floor(Math.random() * (pallete.length - 1)
 function draw() {
 
   translate (width / 2, height / 2);
-  rotate(rotationVal += rotationSpeed);
-  rotationSpeed = Math.sin(Date.now()/10000) * rotationBaseSpeed;
+
+  rotate(rotationVal += rotationBaseSpeed);
+  rotationSpeed = Math.cos(Date.now()/20000) * rotationBaseSpeed;
 
   //if (palleteLoaded) {
 
@@ -118,18 +118,18 @@ function draw() {
     // setGradient(0, 0, width / 2, height, b1, b2, X_AXIS);
     // setGradient(width / 2, 0, width / 2, height, b2, b1, X_AXIS);
 
-
     balls.forEach(ball => {
       ball.collide();
       ball.move();
       ball.display(pg0);
     });
 
-
-
     pg1.copy(pg0,0,Math.floor(height/2),width,Math.floor(height/2),0,0,Math.floor(width),Math.floor(height/2));
 
-    //image(pg0,0,0);
+    // pg1.copy(pg0, 0, Math.floor(height/2), width, Math.floor(height/2), 0, 0, Math.floor(width), Math.floor(100));
+
+
+    // image(pg0,0,0);
     image(pg1, 0, 0);
 
     // translate(240, 0, 0);
@@ -179,14 +179,15 @@ class Ball {
   constructor(xin, yin, din, idin, oin, color, picture) {
     this.x = xin;
     this.y = yin;
-    this.vx = random(4)-2;
+    this.vx = 1000;
     this.vy = 0;
-    this.diameter = random(50);
+    this.diameter = 0.25;
     this.id = idin;
     this.others = oin;
     this.color = color;
+    this.alpha = 0;
     this.picture = picture;
-    this.vRotation = random(0.1) - 0.5;
+    // this.vRotation = random(0.1) - 0.5;
     console.log(this.vRotation);
     this.img = createGraphics(300, 300);
     this.showImg = random();
